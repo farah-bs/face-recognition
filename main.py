d24b83da4b8b6b8508524f1397dda9fb7c41ace7
farah-bs <farah.ben-slama@etu.univ-lyon1.fr>
2025-08-07 15:36:04 +0200

    center menu on screen

--- main.py
+++ main.py
@@ -12,33 +12,48 @@ def start_recognition():
     except Exception as e:
         messagebox.showerror("Error", f"Failed to start recognition:\n{e}")
 
-def threaded_capture(name_entry):
-    name = name_entry.get().strip()
-    if not name:
-        messagebox.showerror("Error", "Please enter a name before capturing.")
-        return
+def start_embedding():
+    try:
+        subprocess.run([sys.executable, "capture_embeddings.py"])
+    except Exception as e:
+        messagebox.showerror("Error", f"Failed to start recognition:\n{e}")
 
-    threading.Thread(target=capture_embeddings_gui, args=(name,), daemon=True).start()
 
 
 # ===============================
 # GUI SETUP
 # ===============================
 root = tk.Tk()
-root.title("Face Recognition System")
-root.geometry("420x280")
+root.title("Live Face Recognition System")
+root.geometry("420x200")
 root.resizable(False, False)
 
+# Center the window on the screen
+window_width = 420
+window_height = 200
+
+# Get screen width and height
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Calculate position x and y coordinates
+position_top = int(screen_height / 2 - window_height / 2)
+position_right = int(screen_width / 2 - window_width / 2)
+
+root.geometry(f"{window_width}x{window_height}+{position_right}+{position_top}")
+
+# Title
 title_label = tk.Label(root, text="Face Recognition System", font=("Helvetica", 18, "bold"))
 title_label.pack(pady=15)
 
 # Buttons
 btn_capture = tk.Button(root, text="📷 Add New Person", font=("Helvetica", 14), width=25,
-                        command=lambda: threaded_capture(name_entry))
+                        command=start_embedding)
 btn_capture.pack(pady=10)
 
 btn_recognize = tk.Button(root, text="🧠 Start Live Recognition", font=("Helvetica", 14), width=25,
                           command=start_recognition)
 btn_recognize.pack(pady=10)
 
+# Start GUI
 root.mainloop()
